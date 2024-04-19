6585ea38f64bb46ac0fdba427ac129c1b9709697
Bone5505 <adamshaw5505@gmail.com>
2024-04-19 23:21:52 +0100

    Reformatted for upload to pip packages

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
-from diffform.release import __version__
+from diffforms.release import __version__
 
-with open("requirements.py") as requirements_file:
-    requirements = requirements_file.read()
+with open('requirements.txt') as req_file:
+    requirements = req_file.read()
 
 setup(
-    name='diffform',
+    name='diffforms',
     version=__version__,
-    authoer="Adam Shaw",
-    packages=['diffform'],
-    description="General symbolic differential form python library",
+    url='https://github.com/Bone5505/Differential-Forms',
+    author="Adam Shaw",
+    packages=['diffforms'],
+    install_requires=requirements,
+    description="Symbolic differential forms python library",
     long_description=""" 
     A symbolic differential form library with exterior calculus operations 
     and sympy integration. Applications involve General Relativity and differential
     form descriptions of Manifolds.
     """,
     keywords="differential forms, sympy, polyforms, exterior derivative",
-    install_equires=requirements,
 )
